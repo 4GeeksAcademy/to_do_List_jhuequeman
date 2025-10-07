d42e7b432c70f928c2a44c6a2b8e26eb7e14f1b4
jhuequeman <huequeman@gmail.com>
2025-10-07 00:59:13 -0300

    Todo list con fetch y react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { RiChatDeleteLine } from "react-icons/ri";
 
@@ -6,20 +6,90 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [tasks, setTasks] = useState([]);
 
+  const tareasdeFetch = ()=>{
+    fetch("https://playground.4geeks.com/todo/users/jhuequeman",{
+      method: "GET",
+      headers:{
+        "Content-Type": "application/json"
+      }
+    })  .then((Response)=> {
+        if(!Response.ok){
+          return crearUsuario().then(() => tareasdeFetch());
+
+    }
+        return Response.json();
+    })
+        .then((data)=> setTasks(data.todos))
+        .catch((error) => console.log(error));
+  
+  }
+
+    const crearUsurio = ()=>{
+      fetch("https://playground.4geeks.com/todo/users/jhuequeman",{
+        method :"POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify({name:"jhuequeman"})
+
+      }) .then((res)=> res.json())
+         .then((data) =>console.log("usuario creado",data))
+         .catch((error)=> console.error("error",error))
+      
+    }
+
+    const agregarTarea = (label)=>{
+      fetch("https://playground.4geeks.com/todo/users/jhuequeman",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body: JSON.stringify({label, is_done: "false"})
+      })
+      
+      .then((res)=> res.json())
+      .then((data)=>{
+        const nuevaTarea = {id:data.id,label}
+        setTasks([... tasks,nuevaTarea]);
+      })
+      .catch((error)=> console.error("error",error));
+      
+
+    }
+
+      const eliminarTarea = (id, index) => {
+    fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+      method: "DELETE"
+    })
+    .then(res => {
+      if (!res.ok) throw new Error("No se pudo eliminar la tarea");
+      const nuevasTareas = tasks.filter((_, i) => i !== index);
+      setTasks(nuevasTareas);
+    })
+    .catch(err => console.log("Error al eliminar tarea:", err));
+  };
+
+  useEffect(()=>{
+    tareasdeFetch();
+  }, [])
+  
+  
+
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleAddTask = (e) => {
     if (e.key === "Enter" && inputValue.trim() !== "") {
-      setTasks([...tasks, inputValue.trim()]);
+      agregarTarea(inputValue.trim());
       setInputValue("");
     }
   };
 
   const deletetodo = (index)=>{
-    const listaActualizada = tasks.filter((FcTodoList, i) => i !== index)
-    setTasks(listaActualizada);
+    const tarea = tasks[index];
+    if(tarea.id){
+         eliminarTarea(tarea.id,index)
+    }
+    
   }
 
   return (
@@ -37,7 +107,7 @@ function App() {
       <ul>
         {tasks.map((t, index) => (
           <li key={index}>
-            {t} <RiChatDeleteLine  onClick={()=>{deletetodo(index)}} className='btn-delete' />
+            {t.label} <RiChatDeleteLine  onClick={()=>{deletetodo(index)}} className='btn-delete' />
           </li>
         ))}
 
